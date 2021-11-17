bf7bf9b32537f6777ef35525355def85f3ea513b
Beuchat Bastien <bastien.beuchat@elca.ch>
2021-11-17 11:11:36 +0100

    [CLOUDTRUST-3881] FIX Migrate from dep to go modules

--- mock_test.go
+++ mock_test.go
@@ -2,4 +2,4 @@ package httpclient
 
 import _ "github.com/golang/mock/mockgen/model"
 
-//go:generate mockgen -destination=./mock/http.go -package=mock -mock_names=Handler=Handler net/http Handler
+//go:generate mockgen --build_flags=--mod=mod -destination=./mock/http.go -package=mock -mock_names=Handler=Handler net/http Handler
