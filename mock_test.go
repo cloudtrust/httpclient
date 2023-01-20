2afdba0e184d3543035f3346bb7cc33f3855b32b
Francis PEROT <francis.perot@elca.ch>
2023-01-20 22:40:37 +0100

    [CLOUDTRUST-4658] Add client for bearer auth according to realm

--- mock_test.go
+++ mock_test.go
@@ -3,3 +3,4 @@ package httpclient
 import _ "github.com/golang/mock/mockgen/model"
 
 //go:generate mockgen --build_flags=--mod=mod -destination=./mock/http.go -package=mock -mock_names=Handler=Handler net/http Handler
+//go:generate mockgen --build_flags=--mod=mod -destination=./mock/token.go -package=mock -mock_names=OidcTokenProvider=OidcTokenProvider github.com/cloudtrust/httpclient OidcTokenProvider
